d69640d18538ada6b5bdad216ffeb0865079c0a7
joaovvsilv <00001132600765sp@al.educacao.sp.gov.br>
2024-09-18 13:55:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+João Vyctor Vieira da Silva 18/09/24 2D
+
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
